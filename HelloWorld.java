283317a256e535a155694212d599d5c22ca025f4
Abdelhak LADJINI <ab.ladjini@gmail.com>
2024-07-24 15:10:27 +0200

    updating project

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-      System.out.println("Hello Abdelhak!");
+      System.out.println("Hello Monsieur LADJINI Abdelhak!");
+      System.out.println("yes i did it");
     }
   }
